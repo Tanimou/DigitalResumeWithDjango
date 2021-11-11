05061f0cd69a4a12fb6df563d9f2357e31bcc968
Tanimou Cisse <tanimou@live.fr>
2021-11-11 21:02:31 +0000

    Fixing

--- resume_demo/main/models.py
+++ resume_demo/main/models.py
@@ -16,6 +16,7 @@ class Skill(models.Model):
     
     def __str__(self):
         return self.name
+    
 
 class UserProfile(models.Model):
 
@@ -27,7 +28,7 @@ class UserProfile(models.Model):
     avatar = models.ImageField(blank=True, null=True, upload_to="avatar")
     title = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    skills = models.ManyToManyField(Skill, blank=True)
+   # skills = models.ManyToManyField(Skill, blank=True)
     cv = models.FileField(blank=True, null=True, upload_to="cv")
 
     def __str__(self):
--- resume_demo/main/views.py
+++ resume_demo/main/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .models import (
-		UserProfile,
-		Blog,
-		Portfolio,
-		Testimonial,
-		Certificate,
-        
-	)
+  
+    Blog,
+    Portfolio,
+    Testimonial,
+    Certificate,
+    Skill,
+)
 
 from django.views import generic
 
@@ -16,29 +16,31 @@ from . forms import ContactForm
 
 
 class IndexView(generic.TemplateView):
-	template_name = "main/index.html"
+    template_name = "main/index.html"
 
+    def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+             
+      testimonials = Testimonial.objects.filter(is_active=True)
+      certificates = Certificate.objects.filter(is_active=True)
+      blogs = Blog.objects.filter(is_active=True)
+      portfolio = Portfolio.objects.filter(is_active=True)
+      skills=Skill.objects.filter(is_key_skill=True)
+                    
+                    
+      context["testimonials"] = testimonials
+      context["certificates"] = certificates
+      context["blogs"] = blogs
+      context["portfolio"] = portfolio
+      context["skills"] = skills
+      return context
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-  
-		testimonials = Testimonial.objects.filter(is_active=True)
-		certificates = Certificate.objects.filter(is_active=True)
-		blogs = Blog.objects.filter(is_active=True)
-		portfolio = Portfolio.objects.filter(is_active=True)
-		
-		context["testimonials"] = testimonials
-		context["certificates"] = certificates
-		context["blogs"] = blogs
-		context["portfolio"] = portfolio
-		return context
-
-
+    
 class ContactView(generic.FormView):
 	template_name = "main/contact.html"
 	form_class = ContactForm
 	success_url = "/"
-	
+
 	def form_valid(self, form):
 		form.save()
 		messages.success(self.request, 'Thank you. We will be in touch soon.')
@@ -58,11 +60,12 @@ class PortfolioDetailView(generic.DetailView):
 	model = Portfolio
 	template_name = "main/portfolio-detail.html"
 
+
 class BlogView(generic.ListView):
 	model = Blog
 	template_name = "main/blog.html"
 	paginate_by = 10
-	
+
 	def get_queryset(self):
 		return super().get_queryset().filter(is_active=True)
 
